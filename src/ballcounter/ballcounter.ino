a4864c2bbcb96c608d440a7e59c7a1357a67eb2e
4dprogramming <frc4Dprogramming@protonmail.com>
2021-11-23 12:51:44 +0300

    added ball counting code

--- src/ballcounter/ballcounter.ino
+++ src/ballcounter/ballcounter.ino
@@ -1,7 +1,11 @@
 int MZ80_A = 7;
 int MZ80_B = 8;
 int snr_a_state = 0;
+int prev_a_state = 0;
 int snr_b_state = 0;
+int prev_b_state = 0;
+int ball_count_a = 0;
+int ball_count_b = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -10,14 +14,23 @@ void setup() {
 }
 
 void loop() {
-  snr_a_state = digitalRead(MZ80_A);
-  snr_b_state = digitalRead(MZ80_A);
-  snr_a_state
-  Serial.print("Sensör 1 : ");
-  Serial.print(snr_a_state);
-  Serial.print("\n");
-  Serial.print("Sensör 2 : ");
-  Serial.print(snr_b_state);
-  Serial.print("--------------\n");
-  delay(500);
+  snr_a_state = !digitalRead(MZ80_A);
+  snr_b_state = !digitalRead(MZ80_A);
+  if(snr_a_state != prev_a_state){
+    if(snr_a_state == 1){
+      ball_count_a++;
+    }
+  }
+  if(snr_b_state != prev_b_state){
+    if(snr_b_state == 1){
+      ball_count_b++;
+    }
+  }
+  Serial.print("1. Sensör Top Sayısı : ");
+  Serial.print(ball_count_a);
+  Serial.print("2. Sensör Top Sayısı : ");
+  Serial.print(ball_count_b);
+  Serial.print("\n--------------\n");
+  prev_a_state = snr_a_state;
+  prev_b_state = snr_b_state;
 }
