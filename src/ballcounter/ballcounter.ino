85dd8aecc5ed5422077743eea6abcbfea4a69324
Egemen Erkin Şenvardarlı <egemen.senvardarli@valensas.com>
2021-11-25 12:00:51 +0300

    Ball counter changed to a function. Added timers for tracking for refresh rate.

--- src/ballcounter/ballcounter.ino
+++ src/ballcounter/ballcounter.ino
@@ -1,9 +1,12 @@
 int MZ80_A = 7;
 int MZ80_B = 8;
 bool sS_1, sS_2, prevState_1, prevState_2 = 0;
-int ball_count = 0;
+int sensor_array[10];
 
-unsigned long time;
+unsigned long time_start;
+unsigned long time_end;
+
+int max_delay = 0;
 
 void setup() {
 	Serial.begin(9600);
@@ -12,17 +15,30 @@ void setup() {
 }
 
 void loop() {
-	time = millis();
-	Serial.println(time);
+	time_start = micros();
 	sS_1 = !digitalRead(MZ80_A);
 	sS_2 = !digitalRead(MZ80_B);
 
-	if(sS_1 != prevState_1 && sS_1 == 1){
-		ball_count++;
-		Serial.print("Sensör 1 Top Sayısı : ");
-		Serial.print(ball_count);
-		Serial.print("\n--------------\n");
-	}
+	count_balls(MZ80_A, sS_1, prevState_1);
+	count_balls(MZ80_B, sS_2, prevState_2);
+
 	prevState_1 = sS_1;
 	prevState_2 = sS_2;
+
+	time_end = micros();
+
+	max_delay = max(max_delay, time_end-time_start);
+
+	Serial.println(max_delay);
+}
+
+void count_balls(int sensor_id, bool sensor, bool prev){
+	if(sensor != prev && sensor == 1){
+		sensor_array[sensor_id]++;
+		Serial.print("Sensor ");
+		Serial.print(sensor_id);
+		Serial.print(" Top Sayısı: ");
+		Serial.print(sensor_array[sensor_id]);
+		Serial.print("\n--------------\n");
+	}
 }
